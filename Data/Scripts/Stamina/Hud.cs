0d915c57ae2f8e2a3f19cf7939cdbd330f4098bc
keyspace <keyspaceksp@gmail.com>
2020-07-31 19:53:07 +0000

    Hud: add StaminaLowOverlay, a (rough!) visual indicator of stamina getting low.

--- Data/Scripts/Stamina/Hud.cs
+++ Data/Scripts/Stamina/Hud.cs
@@ -13,6 +13,7 @@ namespace Keyspace.Stamina
         private HudAPIv2 HudApi;
         private HudAPIv2.HUDMessage hudStaminaReadout;
         private HudAPIv2.BillBoardHUDMessage hudStaminaIcon;
+        private HudAPIv2.BillBoardHUDMessage hudStaminaLowOverlay;
 
         private int stamina;
 
@@ -68,7 +69,7 @@ namespace Keyspace.Stamina
             {
                 hudStaminaReadout = new HudAPIv2.HUDMessage(
                     Message: new StringBuilder(COLOR_STRING_Y.Length + "100".Length),
-                    Origin: new Vector2D(-0.925, 0.95),
+                    Origin: new Vector2D(-0.925d, 0.95d),
                     Blend: BlendTypeEnum.PostPP
                     );
             }
@@ -79,22 +80,47 @@ namespace Keyspace.Stamina
                 float aspectRatio = viewportSize.X / viewportSize.Y;
                 hudStaminaIcon = new HudAPIv2.BillBoardHUDMessage(
                     Material: MyStringId.GetOrCompute("StaminaIconMale"),
-                    Origin: new Vector2D(-0.95, 0.935),
+                    Origin: new Vector2D(-0.95d, 0.935d),
                     BillBoardColor: Color.White,
-                    Width: 0.0325f / (float)aspectRatio,
+                    Width: 0.0325f / aspectRatio,
                     Height: 0.0325f,
                     Blend: BlendTypeEnum.PostPP
+                    );
+            }
+            if (hudStaminaLowOverlay == null)
+            {
+                hudStaminaLowOverlay = new HudAPIv2.BillBoardHUDMessage(
+                    Material: MyStringId.GetOrCompute("StaminaLowOverlay"),
+                    Origin: new Vector2D(0.0d, 0.0d),
+                    BillBoardColor: new Color(new Vector4(0.0f, 0.0f, 0.0f, 0.5f)),
+                    Width: 2.0f,
+                    Height: 2.0f,
+                    Blend: BlendTypeEnum.LDR
                     );
             }
 
             hudStaminaReadout.Message.Clear();
             
             if (stamina > 75)
+            {
                 colorString = COLOR_STRING_W;
+            }
             else if (stamina < 25)
+            {
                 colorString = COLOR_STRING_R;
+                float alpha = -stamina / 25.0f + 1.0f;
+                alpha = Math.Min(1.0f, Math.Max(0.0f, alpha));
+                hudStaminaLowOverlay.BillBoardColor = new Color(new Vector4(1.0f, 1.0f, 1.0f, alpha));
+                // DEBUG
+                var msg = $"{alpha}";
+                //MyLog.Default.WriteLineAndConsole(msg);
+                MyAPIGateway.Utilities.ShowNotification(msg, 1000);
+            }
             else
+            {
                 colorString = COLOR_STRING_Y;
+                hudStaminaLowOverlay.BillBoardColor = new Color(new Vector4(0.0f, 0.0f, 0.0f, 0.0f));
+            }
 
             hudStaminaReadout.Message.AppendFormat($"{colorString}{stamina}");
 
